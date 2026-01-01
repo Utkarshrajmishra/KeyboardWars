7c554ea16c32a823889277932e3213b742aaf04a
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-01-01 13:14:36 +0530

    Fix Auth

--- auth.ts
+++ auth.ts
@@ -7,8 +7,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
         GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   // callbacks: {
